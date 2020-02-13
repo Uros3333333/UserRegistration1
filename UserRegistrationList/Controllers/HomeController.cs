e1daf188f89c1cfa6aa6717d7c46a3f164f9a933
Uroš Stupar <uros.stupar@gmail.com>
2020-02-13 20:20:35 +0100

    changed

---
+++ UserRegistrationList/Controllers/HomeController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UserRegistrationList.Models;
+using DataLibrary;
+using DataLibrary.Logic;
+
+
+namespace UserRegistrationList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult NewUser()
+
+        {
+            ViewBag.Message = "User sign-up";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult NewUser(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int recordsCreated = UserProcessor.CreateUser(model.FirstName, model.LastName, model.EmailAddress);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult UserList()
+        {
+            ViewBag.Message = "User List";
+
+            
+            var data = UserProcessor.LoadUserList();
+            List<UserModel> users = new List<UserModel>();
+            int x = 1;
+
+
+            /*    foreach (var item in data)
+                {
+                    users.Add(new UserModel()
+                    {
+                        Id = item.Id,
+                        FirstName = item.FirstName,
+                        LastName = item.LastName,
+                        EmailAddress = item.EmailAddress,
+                        ConfirmEmailAddress = item.EmailAddress
+
+                    });
+            
+
+
+
+                }*/
+            foreach (var item in data)
+            {
+                UserModel model = new UserModel();
+                model.Id = item.Id;
+                model.FirstName = item.FirstName;
+                model.LastName = item.LastName;
+                model.EmailAddress = item.EmailAddress;
+                model.ConfirmEmailAddress = item.EmailAddress;
+
+                users.Add(model);
+                x += 1;
+            }
+            return View(users);
+
+            }
+
+            
+
+       
+
+           
+
+
+
+
+
+    
+
+        public IActionResult Proba()
+
+           
+        {
+
+            var data = UserProcessor.LoadUserList();
+            Console.WriteLine(data);
+            return View();
+        }
+
+    }
+}
+
+
---
+++ UserRegistrationList/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegistrationList.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please enter your last name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please enter your e-mail address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm e-mail Address")]
+        [DataType(DataType.EmailAddress)]
+        [Compare("EmailAddress", ErrorMessage = "e-mail addresses do not match")]
+        public string ConfirmEmailAddress { get; set; }
+    }
+}
